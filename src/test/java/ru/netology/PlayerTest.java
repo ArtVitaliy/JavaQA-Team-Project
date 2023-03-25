42b5e8de08e0b9d40ee7549d866b6f77bb8547e3
ArtVitaly <0331116@mail.ru>
2023-03-25 12:55:05 +0300

    fixed Player and GameStore

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -1,7 +1,10 @@
 package ru.netology;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlayerTest {
 
@@ -19,5 +22,122 @@ public class PlayerTest {
         assertEquals(expected, actual);
     }
 
-    // другие ваши тесты
-}
+
+    @Test
+    public void shouldSumGenreIfTwoGame() {
+        GameStore store = new GameStore();
+        Game MK = store.publishGame("Mortal Combat", "Аркады");
+        Game lbp = store.publishGame("LBP", "Приключение");
+        Game rdr = store.publishGame("RDR", "Приключение");
+        Game rayman = store.publishGame("Rayman", "Аркады");
+
+
+        Player player = new Player("Ivan");
+
+        player.installGame(MK);
+        player.installGame(rayman);
+        player.installGame(lbp);
+        player.installGame(rdr);
+
+        player.play(MK, 3);
+        player.play(rayman, 5);
+        player.play(lbp, 10);
+        player.play(rdr, 20);
+
+
+        int expected = 8;
+        int actual = player.sumGenre("Аркады");
+//        int actual = player.sumGenre(game.getGenre());
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void playTestRuntimeException() {
+
+        GameStore store = new GameStore();
+        Game MK = store.publishGame("Mortal Combat", "Аркады");
+        Game lbp = store.publishGame("LBP", "Приключение");
+        Game rdr = store.publishGame("RDR", "Приключение");
+        Game rayman = store.publishGame("Rayman", "Аркады");
+
+        Player player = new Player("Ivan");
+
+        player.installGame(rayman);
+        player.installGame(lbp);
+        player.installGame(rdr);
+
+        player.play(rayman, 5);
+        player.play(lbp, 10);
+        player.play(rdr, 20);
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            player.play(MK, 4);
+        });
+
+
+    }
+
+    @Test
+    public void testMostPlayerByGenre() {
+        GameStore store = new GameStore();
+
+        Game MK = store.publishGame("Mortal Combat", "Аркады");
+        Game lbp = store.publishGame("LBP", "Приключение");
+        Game rdr = store.publishGame("RDR", "Приключение");
+        Game rayman = store.publishGame("Rayman", "Аркады");
+
+        Player player = new Player("Ivan");
+
+        player.installGame(MK);
+        player.installGame(rayman);
+        player.installGame(lbp);
+        player.installGame(rdr);
+
+
+        player.play(MK, 3);
+        player.play(rayman, 5);
+        player.play(lbp, 10);
+        player.play(rdr, 20);
+
+        Game expected = rdr;
+        Game actually = player.mostPlayerByGenre("Приключение");
+        Assertions.assertEquals(expected, actually);
+
+
+
+    }
+    @Test
+    public void testMostPlayerByGenreNull() {
+        GameStore store = new GameStore();
+
+        Game MK = store.publishGame("Mortal Combat", "Аркады");
+        Game lbp = store.publishGame("LBP", "Приключение");
+        Game rdr = store.publishGame("RDR", "Приключение");
+        Game rayman = store.publishGame("Rayman", "Аркады");
+        Game ecco = store.publishGame("Ecco", "Шутер");
+
+        Player player = new Player("Ivan");
+
+        player.installGame(MK);
+        player.installGame(rayman);
+        player.installGame(lbp);
+        player.installGame(rdr);
+        player.installGame(ecco);
+
+
+        player.play(MK, 3);
+        player.play(rayman, 5);
+        player.play(lbp, 10);
+        player.play(rdr, 20);
+
+        Game expected = null;
+        Game actually = player.mostPlayerByGenre("Шутер");
+        Assertions.assertEquals(expected, actually);
+    }
+
+
+    }
+
+
+// другие ваши тесты
+
